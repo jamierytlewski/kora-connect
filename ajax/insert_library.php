0cf483693314ef12b0a5ce54fea251f7d5d91a4e
Jamie R Rytlewski <jamier.net@gmail.com>
2014-04-17 22:40:30 -0400

    SQL Injection
    https://codex.wordpress.org/Class_Reference/wpdb
    http://ottopress.com/2013/better-know-a-vulnerability-sql-injection/

--- ajax/insert_library.php
+++ ajax/insert_library.php
@@ -4,18 +4,18 @@
 
 	if(isset($_GET['chk'])){
 		//first we check that object is not already at the library
-		
+
 		$library= $wpdb->prefix . 'koralibrary';
 
 		$kid = $_GET['chk'];
-		$query="SELECT * FROM  $library where KID='$kid'";
+		$query="SELECT * FROM  $library where KID=%d";
 		//check first that the object is not in the library.
-		if(empty($wpdb->get_results($query))){
-		
+		if(empty($wpdb->get_results($wpdb->prepare($query), $kid))){
+
 			$token = get_option('kordat_dbtoken');
 			$pid = get_option('kordat_dbproj');
 			$sid = get_option('kordat_dbscheme');
-			$user = get_option('kordat_dbuser');  
+			$user = get_option('kordat_dbuser');
 			$pass = get_option('kordat_dbpass');
 			$query = "KID,=,".$kid;
 			$restful_url = get_option('kordat_dbapi') . KORA_PLUGIN_RESTFUL_SUBPATH;
@@ -24,7 +24,7 @@
 
 			///build url
 			$url = $restful_url.'?request=GET&pid='.$pid.'&sid='.$sid.'&token='.$token.'&display='.urlencode($display).'&fields='.urlencode($fields).'&query='.urlencode($query);
-			
+
 			///initialize post request to KORA API using curl
 			$ch = curl_init($url);
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -32,7 +32,7 @@
 
 			///capture results and display
 			$server_output = curl_exec($ch);
-			
+
 
 			$xpath = new DOMXPath(@DOMDocument::loadHTML($server_output));
 			$thumb_src = $xpath->evaluate("string(//img/@src)");
@@ -55,7 +55,7 @@
 			echo json_encode (true);
 		}else{
 			echo json_encode(false);
-		
+
 		}
 	}
 ?>
